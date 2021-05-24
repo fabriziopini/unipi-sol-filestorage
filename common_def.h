884cd6f14bfba53da9ccafd0c53a58498059220e
Fabrizio Pini <fabrizio.pini96@gmail.com>
2021-05-24 14:56:13 +0200

    aggiunti ifndef

--- common_def.h
+++ common_def.h
@@ -1,27 +1,32 @@
-#define MAX_PATH_LENGTH 256
+#ifndef COMMON_DEF_H
+#define COMMON_DEF_H
+
+#define UNIX_PATH_MAX 104 
 
 typedef enum {
-    O_NULL =        0,
-    O_CREATE =      1,
-    O_LOCK =        2,
+    O_NULL        = 0,
+    O_CREATE      = 1,
+    O_LOCK        = 2,
     O_CREATE_LOCK = 3
 } file_flag;
 
 typedef enum {
+    NOTHING = 0,
     OPENFILE = 1,
     READFILE = 2,
-    WRITEFILE = 3,
-    APPENDTOFILE = 4,
-    LOCKFILE = 5,
-    UNLOCKFILE = 6,
-    CLOSEFILE = 7,
-    REMOVEFILE = 8
+    READNFILES = 3,
+    WRITEFILE = 4,
+    APPENDTOFILE = 5,
+    LOCKFILE = 6,
+    UNLOCKFILE = 7,
+    CLOSEFILE = 8,
+    REMOVEFILE = 9
 
 } op_t;
 
 typedef struct {
     op_t            op;
-    char            pathname[MAX_PATH_LENGTH];
+    char            pathname[UNIX_PATH_MAX];
     int             flag; //TODO file_flag al posto di int?
     int             datalen;
 } msg_request_t;
@@ -30,3 +35,5 @@ typedef struct {
     int result; // 0:success, >0: errno
     int datalen;
 } msg_response_t;
+
+#endif
--- common_funcs.h
+++ common_funcs.h
@@ -1,3 +1,6 @@
+#ifndef COMMON_FUNCS_H
+#define COMMON_FUNCS_H
+
 #include <sys/types.h>
 
 /* Read "n" bytes from a descriptor */
@@ -5,3 +8,5 @@ ssize_t readn(int fd, void *ptr, size_t n);
 
 /* Write "n" bytes to a descriptor */
 ssize_t writen(int fd, void *ptr, size_t n);
+
+#endif
