f35a4c0cb7c80a98aaf3997d648e10e26e3f4b22
Fabrizio Pini <fabrizio.pini96@gmail.com>
2021-06-01 11:24:23 +0200

    added createWriteInDir

--- common_funcs.c
+++ common_funcs.c
@@ -1,6 +1,15 @@
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 //TODO altri include??
+
+#ifndef COMMON_FUNCS_H
+#define COMMON_FUNCS_H
 
 /* Read "n" bytes from a descriptor */
 ssize_t readn(int fd, void *ptr, size_t n) {  
@@ -36,3 +45,54 @@ ssize_t writen(int fd, void *ptr, size_t n) {
     }
     return(n - nleft); /* return >= 0 */
 }
+
+char *getFileName(const char *path)
+{
+    char *filename = strrchr(path, '\/'); // \\ su windows
+    if (filename == NULL)
+        filename = path;
+    else
+        filename++;
+    return filename;
+}
+
+
+int createWriteInDir (char *pathname, void *buf, size_t size, char *dirname) {
+
+    printf("CreateWriteDir\n");
+    char *filename = getFileName(pathname);
+    int len = strlen(filename) + strlen(dirname) + 1;
+    char *final_pathname = malloc(len);
+    if (final_pathname == NULL) return -1;
+
+    strcpy(final_pathname, dirname);
+    strcat(final_pathname,"/");
+    strcat(final_pathname, filename);
+
+    printf("pathname: %s\n",final_pathname);
+    fflush(stdout);
+
+    int fd = open(final_pathname, O_WRONLY|O_CREAT|O_TRUNC, 0666); // file descriptor
+    free(final_pathname);
+    if (fd == -1) {
+        perror("s.c, in apertura (creazione)");
+        return -1;
+    }
+
+    if (write(fd, buf, size) == -1) {
+        perror("s.c: write"); 
+        return -1;
+    }
+
+    if (close(fd) == -1) {
+        perror("s.c: close");
+        return -1;
+    }
+
+    printf("fine createwriteDir\n");
+    fflush(stdout);
+
+    return 0;
+}
+
+#endif
--- common_funcs.h
+++ common_funcs.h
@@ -9,4 +9,13 @@ ssize_t readn(int fd, void *ptr, size_t n);
 /* Write "n" bytes to a descriptor */
 ssize_t writen(int fd, void *ptr, size_t n);
 
+/**
+ * Creo e scrivo il file di contenuto buf in dirname
+ * 
+ * \param pathname path assoluto del file (è stato salvato così sul server)
+ * 
+ * 
+ */
+int createWriteInDir (char *pathname, void *buf, size_t size, char *dirname);
+
 #endif
